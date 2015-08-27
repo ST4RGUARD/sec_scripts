95b86df2a5728d0b575253f35d2c861082cd88da
ma11ock <thema11ock@gmail.com>
2015-08-27 14:04:34 -0400

    initial portscanner commit - buggy + needs refactoring

---
+++ python/Module-2/portscanner.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+
+__author__ = 'ma11ock'
+import socket
+import sys
+import _thread
+from datetime import datetime
+
+
+server = input("Enter host to scan: ")
+server_ip = socket.gethostbyname(server)
+
+if sys.argv[1] == "-h" or sys.argv[1] == "--help":
+    print("enter port range to scan -> 0 100")
+
+elif(0 <= int(sys.argv[1]) <= 65536) \
+        and (int(sys.argv[1]) <= int(sys.argv[2])) \
+        and (0 <= int(sys.argv[2]) <= 65536):
+        first_port = int(sys.argv[1])
+        last_port  = int(sys.argv[2])
+else:
+    print("invalid input")
+    # term script
+
+
+print("-" * 60)
+print("scanning host", server_ip)
+print("-" * 60)
+
+time_started = datetime.now()
+
+
+def create_threads():
+
+    ports = last_port - first_port
+    mod_ports = ports % 10
+
+    if mod_ports > 0:
+        ports -= mod_ports
+        _thread.start_new_thread(scanning(ports, last_port))
+        for port in range(first_port, ports, 10):
+            _thread.start_new_thread(scanning, (first_port, ports), (port,))
+    else:
+        for port in range(first_port, last_port, 10):
+            if ports >= 10:
+                end_port = port + 10
+            _thread.start_new_thread(scanning, (port, end_port))
+
+
+def scanning(begin, end):
+    for port in range(begin, end):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        result = sock.connect_ex((server_ip, port))
+
+        if result == 0:
+            print("Port {}: \t Open".format(port))
+
+        sock.close()
+
+create_threads()
+
+time_now = datetime.now()
+
+# Calculates the difference of time, to see how long it took to run the script
+total = time_now - time_started
+
+# Printing the information to screen
+print("Scanning Completed in: ", total)

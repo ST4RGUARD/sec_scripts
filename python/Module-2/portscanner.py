22b5a225692ca5a71431ddd04cff6ae569aa5a4b
ma11ock <thema11ock@gmail.com>
2015-08-28 14:27:39 -0400

    2nd revision to portscanner

--- python/Module-2/portscanner.py
+++ python/Module-2/portscanner.py
@@ -5,20 +5,29 @@ import socket
 import sys
 import _thread
 
-server = input("Enter host to scan: ")
-server_ip = socket.gethostbyname(server)
+try:
+    server = input("Enter host to scan: ")
+    server_ip = socket.gethostbyname(server)
+except socket.gaierror:
+    sys.exit("server name not known")
 
-if sys.argv[1] == "-h" or sys.argv[1] == "--help":
-    print("enter port range to scan -> 0 100")
 
-elif(0 <= int(sys.argv[1]) <= 65536) \
-        and (int(sys.argv[1]) <= int(sys.argv[2])) \
-        and (0 <= int(sys.argv[2]) <= 65536):
-        first_port = int(sys.argv[1])
-        last_port  = int(sys.argv[2])
-else:
-    print("invalid input")
-    # term script
+# give better options later
+try:
+    if sys.argv[1] == "-h" or sys.argv[1] == "--help":
+        print("enter port range to scan -> 0 100")
+
+    # refactor this later
+    elif(0 <= int(sys.argv[1]) <= 65536) \
+            and (int(sys.argv[1]) <= int(sys.argv[2])) \
+            and (0 <= int(sys.argv[2]) <= 65536):
+            first_port = int(sys.argv[1])
+            last_port  = int(sys.argv[2])
+    else:
+        sys.exit("invalid input!")
+
+except ValueError:
+    sys.exit("invalid input!")
 
 
 print("-" * 60)
@@ -35,7 +44,7 @@ def create_threads():
         ports -= mod_ports
         _thread.start_new_thread(scanning(ports, last_port))
         for port in range(first_port, ports, 10):
-            _thread.start_new_thread(scanning, (first_port, ports), (port,))
+            _thread.start_new_thread(scanning, (first_port, ports))
     else:
         for port in range(first_port, last_port, 10):
             if ports >= 10:
@@ -43,13 +52,13 @@ def create_threads():
             _thread.start_new_thread(scanning, (port, end_port))
 
 
-def scanning(begin, end):
-    for port in range(begin, end):
+def scanning(first, last):
+    for port in range(first, last+1):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         result = sock.connect_ex((server_ip, port))
 
         if result == 0:
-            print("Port {}: \t Open".format(port))
+            print("port {}: \t open".format(port))
 
         sock.close()
 

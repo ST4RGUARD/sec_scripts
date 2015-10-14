e71625e7718a7efd3e9778694832687fd204dd39
ma11ock <thema11ock@gmail.com>
2015-10-14 09:28:08 -0400

    meaningless changes

--- ruby/pcap2json.rb
+++ ruby/pcap2json.rb
@@ -1,23 +1,23 @@
-require 'JSON'             # => true
-require 'cobravsmongoose'  # => true
-require 'awesome_print'    # => true
-require 'nokogiri'         # => true
+require 'JSON'
+require 'cobravsmongoose'
+require 'awesome_print'
+require 'nokogiri'
 
-if ARGV[0] == "-h" || ARGV[0] == "--help" || ARGV[0] == nil                            # => true
-  puts "  | ruby pcap2json.rb INPUT OUTPUT"                                            # => nil
-  puts "  | ruby pcap2json.rb /Users/BOB/Desktop/pcap.pcapng /Users/BOB/Desktop/json"  # => nil
-  exit                                                                                 # ~> SystemExit: exit
+if ARGV[0] == "-h" || ARGV[0] == "--help" || ARGV[0] == nil
+  puts "  | ruby pcap2json.rb INPUT OUTPUT"
+  puts "  | ruby pcap2json.rb /Users/BOB/Desktop/pcap.pcapng /Users/BOB/Desktop/json"
+  exit
 else
   #change ARGV[1] to xml to convert to json
-  `tshark -r "#{ARGV[0]}" -T pdml > "#{ARGV[1]}"`
+  `tshark -r "#{ARGV[0]}" -T pdml > xml`
 end
 
 # 1 method
-#xml = File.open("xml").read
+xml = File.open("xml").read
 #json = CobraVsMongoose.xml_to_json(xml)
 #pretty_json = JSON.pretty_generate(JSON.parse json)
 #output = ap pretty_json
-#File.open("#{ARGV[1]}", "w") { |file| file.write(json) }
+File.open("#{ARGV[1]}", "w") { |file| file.write(xml) }
 
 
 # 2 method
@@ -36,18 +36,9 @@ end
 
 #`rm xml`
 
-f = File.open("#{ARGV[1]}")
-doc = Nokogiri::XML(f)
+#f = File.open("#{ARGV[1]}")
+#doc = Nokogiri::XML(f)
 
-puts doc.xpath('//proto/name/@showname')
+#puts doc.xpath('//proto/name/@showname')
 
-f.close
-
-# >>   | ruby pcap2json.rb INPUT OUTPUT
-# >>   | ruby pcap2json.rb /Users/BOB/Desktop/pcap.pcapng /Users/BOB/Desktop/json
-
-# ~> SystemExit
-# ~> exit
-# ~>
-# ~> /var/folders/qj/6ctp06sn5r379j9nz81gz_xc0000gp/T/seeing_is_believing_temp_dir20150727-85109-1ulumsk/program.rb:9:in `exit'
-# ~> /var/folders/qj/6ctp06sn5r379j9nz81gz_xc0000gp/T/seeing_is_believing_temp_dir20150727-85109-1ulumsk/program.rb:9:in `<main>'
+#f.close

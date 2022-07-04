04fdfcb99189edce20217f48dd95a8547263334b
ST4RGUARD <thema11ock@gmail.com>
2022-07-04 08:55:02 -0500

    remove double = condition

--- go/src/ninja3/exercise8.go
+++ go/src/ninja3/exercise8.go
@@ -5,11 +5,12 @@ import (
 )
 
 func exercise8() {
+	num := 1
 	//switch statement with no condition
 	switch {
-	case 1==1:
+	case num == 1:
 		fmt.Println("TRUE")
-	case 1==2:
+	case 1 == 2:
 		fmt.Println("FALSE")
 	default:
 		fmt.Println("default")

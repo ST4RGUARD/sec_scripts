9baf444155cd97368088ee5e6b36da51ec970957
ST4RGUARD <thema11ock@gmail.com>
2022-07-22 15:01:55 -0500

    concurrency

--- go/src/ninja9/ninja9Lessons.go
+++ go/src/ninja9/ninja9Lessons.go
@@ -1,5 +1,93 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"runtime"
+	"sync"
+	"sync/atomic"
+)
 
+var wg sync.WaitGroup
+
+func foo() {
+	for i := 0; i < 10; i++ {
+		fmt.Println("foo:", i)
+	}
+	wg.Done()
+}
+
+func bar() {
+	for i := 0; i < 10; i++ {
+		fmt.Println("foo:", i)
+	}
+}
+
+func race() {
+	fmt.Println("RACE")
+	fmt.Println("--------")
+	fmt.Println("CPUs:", runtime.NumCPU())
+	fmt.Println("Goroutines:", runtime.NumGoroutine())
+	counter := 0
+	const gs = 100
+
+	var wg sync.WaitGroup
+	wg.Add(gs)
+	var mu sync.Mutex
+	for i := 0; i < gs; i++ {
+		go func() {
+			mu.Lock()
+			v := counter
+			// Gosched()?
+			runtime.Gosched()
+			v++
+			counter = v
+			mu.Unlock()
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	fmt.Println("Goroutines:", runtime.NumGoroutine())
+	fmt.Println("counter:", counter)
+}
+
+func atomics() {
+	fmt.Println("RACE")
+	fmt.Println("--------")
+	fmt.Println("CPUs:", runtime.NumCPU())
+	fmt.Println("Goroutines:", runtime.NumGoroutine())
+
+	var counter int64
+	const gs = 100
+
+	var wg sync.WaitGroup
+	wg.Add(gs)
+	for i := 0; i < gs; i++ {
+		go func() {
+			atomic.AddInt64(&counter, 1)
+			runtime.Gosched()
+			fmt.Println("Counter:", atomic.LoadInt64(&counter))
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	fmt.Println("Goroutines:", runtime.NumGoroutine())
+	fmt.Println("counter:", counter)
+}
+
+func main() {
+	fmt.Println("OS", runtime.GOOS)
+	fmt.Println("ARCH", runtime.GOARCH)
+	fmt.Println("CPUS", runtime.NumCPU())
+	fmt.Println("GOROUTINES", runtime.NumGoroutine())
+
+	wg.Add(1)
+	go foo()
+	bar()
+
+	fmt.Println("CPUS", runtime.NumCPU())
+	fmt.Println("GOROUTINES", runtime.NumGoroutine())
+	wg.Wait()
+
+	//race()
+	atomics()
 }

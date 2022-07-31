189724b7bc487029f9f03db63e3ee541b33f74f3
ST4RGUARD <thema11ock@gmail.com>
2022-07-31 13:50:06 -0500

    modules

---
+++ ruby/Comprehensive Ruby - Udemy/section 9 OOP/modules.rb
@@ -0,0 +1,46 @@
+# class Sale
+#   def initialize(params)
+#     @params = params
+#   end
+
+#   def call
+#     puts "Prams in class: #{@params}"
+#   end
+# end
+
+# sale = Sale.new("Class params")
+# sale.call
+
+module Sale
+  module FormBuilder
+    class << self
+      def call(params)
+        subtotal = params[:subtotal]
+        state_name = params[:state_name]
+
+        tax_amount = subtotal * self.tax_rate(state_name)
+        subtotal + tax_amount
+      end
+
+    private
+      def tax_rate(state)
+        case state
+        when 'AZ'
+          0.065
+        when 'CA'
+          0.087
+        end
+      end
+    end
+  end
+end
+
+# Sale.call("test params")
+# Sale::FormBuilder.call("test params")
+
+form_data = {
+  subtotal: 5.5,
+  state_name: 'AZ'
+}
+
+ Sale::FormBuilder.call(form_data)

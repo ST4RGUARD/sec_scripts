5d6b4e4d336f3fe53e5e7d1b7ce2044fc2c67aec
ST4RGUARD <thema11ock@gmail.com>
2025-06-19 11:21:25 -0500

    making sure rubyfmt is installed

--- ruby/buzzyfizzy.rb
+++ ruby/buzzyfizzy.rb
@@ -1,58 +1,69 @@
-for i in 1..100 do
-  print "#{i}:"
+for i in 1..100
+  print("#{i}:")
   if i % 5 == 0 && i % 3 == 0
-    puts 'fizzbuzz'
+    puts("fizzbuzz")
   elsif i % 3 == 0
-    puts 'fizz'
+    puts("fizz")
   elsif i % 5 == 0
-    puts 'buzz'
+    puts("buzz")
   end
 end
 
 1.upto(100) do |num|
-  puts num.to_s
+  puts(num.to_s)
   if num % 15 == 0
-    puts 'fizzbuzz'
+    puts("fizzbuzz")
   elsif num % 3 == 0
-    puts 'fizz'
+    puts("fizz")
   elsif num % 5 == 0
-    puts 'buzz'
+    puts("buzz")
   end
 end
 
 (1..100).each do |num|
-  puts num.to_s + ' : ' + (if num % 15 == 0
-                             'fizzbuzz'
-                           else
-                             (if num % 3 == 0
-                                'fizz'
-                              else
-                                (num % 5 == 0 ? 'buzz' : '')
-                              end
-                             )
-                           end)
+  puts(
+    num.to_s +
+      " : " +
+      (if num % 15 == 0
+        "fizzbuzz"
+      else
+        (if num % 3 == 0
+          "fizz"
+        else
+          (num % 5 == 0 ? "buzz" : "")
+        end)
+      end)
+  )
 end
 
 1.upto(100) do |num|
-  puts num.to_s + ' : ' + (if num % 15 == 0
-                             'fizzbuzz'
-                           else
-                             (if num % 3 == 0
-                                'fizz'
-                              else
-                                (num % 5 == 0 ? 'buzz' : '')
-                              end)
-                           end)
+  puts(
+    num.to_s +
+      " : " +
+      (if num % 15 == 0
+        "fizzbuzz"
+      else
+        (if num % 3 == 0
+          "fizz"
+        else
+          (num % 5 == 0 ? "buzz" : "")
+        end)
+      end)
+  )
 end
 
 1.step(100, 1) do |num|
-  puts num.to_s + ' : ' + (if num % 15 == 0
-                             'fizzbuzz'
-                           else
-                             (if num % 3 == 0
-                                'fizz'
-                              else
-                                (num % 5 == 0 ? 'buzz' : '')
-                              end)
-                           end)
+  puts(
+    num.to_s +
+      " : " +
+      (if num % 15 == 0
+        "fizzbuzz"
+      else
+        (if num % 3 == 0
+          "fizz"
+        else
+          (num % 5 == 0 ? "buzz" : "")
+        end)
+      end)
+  )
 end
